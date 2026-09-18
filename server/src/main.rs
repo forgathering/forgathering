8b57d782a32221acd9a4bb604b25450bf65ca5a1
FizzyApple12 <github@fizzyapple12.com>
2026-09-17 20:47:17 -0400

    create testing webserver to check hosting functionality

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body(
+        "
+    <!DOCTYPE html>
+    <html lang=\"en\">
+    <head>
+        <meta charset=\"UTF-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+        <title>forgather</title>
+    </head>
+    <body>
+        <h1>hi... #hitwt</h1>
+    </body>
+    </html>
+    ",
+    )
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("0.0.0.0", 3000))?
+        .run()
+        .await
 }
